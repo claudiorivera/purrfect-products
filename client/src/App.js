7cd233c6fba785c58100ff5da06ba51f5677be6a
Claudio Rivera <me@claudiorivera.com>
2020-08-22 15:19:20 -0500

    basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,62 @@
 import React from "react";
 import "./App.css";
-import data from "./data";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button
+              onClick={() => {
+                document.querySelector(".sidebar").classList.add("open");
+              }}
+            >
+              &#9776;
+            </button>
+            <Link to="/">Purr-fect Products!</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
           <button
+            className="sidebar-close-button"
             onClick={() => {
-              document.querySelector(".sidebar").classList.add("open");
+              document.querySelector(".sidebar").classList.remove("open");
             }}
           >
-            &#9776;
+            X
           </button>
-          <a href="index.html">Purr-fect Products!</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button
-          className="sidebar-close-button"
-          onClick={() => {
-            document.querySelector(".sidebar").classList.remove("open");
-          }}
-        >
-          X
-        </button>
-        <ul>
-          <li>
-            <a href="#">Toys</a>
-          </li>
-          <li>
-            <a href="#">Food</a>
-          </li>
-          <li>
-            <a href="#">Miscellaneous</a>
-          </li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            {data.products.map((product) => (
-              <li>
-                <div className="product">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                  <div className="product-name">
-                    <a href="#">{product.name}</a>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">${product.price}</div>
-                  <div className="product-rating">
-                    {product.rating} Stars ({product.numReviews} Reviews)
-                  </div>
-                </div>
-              </li>
-            ))}
+          <ul>
+            <li>
+              <a href="#">Toys</a>
+            </li>
+            <li>
+              <a href="#">Food</a>
+            </li>
+            <li>
+              <a href="#">Miscellaneous</a>
+            </li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">
-        All rights reserved <span>😻</span>
-      </footer>
-    </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products/:id" component={ProductPage} />
+            <Route path="/" component={HomePage} exact />
+          </div>
+        </main>
+        <footer className="footer">
+          All rights reserved <span>😻</span>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
