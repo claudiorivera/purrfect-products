f987e9b38be9c184e295d8a964f911f55207e5a2
Claudio Rivera <me@claudiorivera.com>
2020-08-22 14:44:49 -0500

    Begin react frontend

---
+++ client/src/data.js
@@ -0,0 +1,31 @@
+export default {
+  products: [
+    {
+      name: "Botanicals Catnip Lavender",
+      image: "/images/botanicals-catnip-lavender.jpg",
+      price: 10,
+      brand: "Kong",
+      rating: 4.5,
+      numReviews: 12,
+      category: "Miscellaneous",
+    },
+    {
+      name: "Air Prey Telescoping Wand",
+      image: "/images/air-prey-telescoping-wand.jpg",
+      price: 12,
+      brand: "Jackson Galaxy",
+      rating: 4.6,
+      numReviews: 13,
+      category: "Toys",
+    },
+    {
+      name: "Country Dinner",
+      image: "/images/country-style-dinner.jpg",
+      price: 1,
+      brand: "Friskies",
+      rating: 4.2,
+      numReviews: 23,
+      category: "Food",
+    },
+  ],
+};
