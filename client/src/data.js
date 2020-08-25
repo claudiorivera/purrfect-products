e2f95393af86fe7090bf0348bb948c0edeebe1ce
Claudio Rivera <me@claudiorivera.com>
2020-08-24 19:49:48 -0500

    added mock info

--- client/src/data.js
+++ client/src/data.js
@@ -7,8 +7,11 @@ export default {
       price: 10,
       brand: "Kong",
       rating: 4.5,
-      numReviews: 12,
+      numberOfReviews: 12,
       category: "Miscellaneous",
+      status: "in stock",
+      description:
+        "KONG Botanicals Lavender Herbal catnip Infusion creates an aura of calm for your cat. The enticing natural aromatic blend of premium catnip will bring blissful tranquility to naptime and provide comfort while your cat is idling away an afternoon. An attractive option to use with KONG cat toys or on its own.",
     },
     {
       _id: 2,
@@ -17,8 +20,11 @@ export default {
       price: 12,
       brand: "Jackson Galaxy",
       rating: 4.6,
-      numReviews: 13,
+      numberOfReviews: 13,
       category: "Toys",
+      status: "in stock",
+      description:
+        "Get your frisky feline pouncing and jumping after this soaring feather toy. Designed by Jackson Galaxy to engage your cat in play that appeals to their natural instincts by simulating the hunt.",
     },
     {
       _id: 3,
@@ -27,8 +33,11 @@ export default {
       price: 1,
       brand: "Friskies",
       rating: 4.2,
-      numReviews: 23,
+      numberOfReviews: 23,
       category: "Food",
+      status: "in stock",
+      description:
+        "The enticing flavor of chicken, plus a smooth, pleasing texture will leave your cat purrfectly satisfied.",
     },
   ],
 };
