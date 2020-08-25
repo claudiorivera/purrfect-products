e39d3c5b051e3b8f7cf0a53abf847edf796903ee
Claudio Rivera <me@claudiorivera.com>
2020-08-24 19:50:05 -0500

    product details page

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { Fragment } from "react";
+import data from "../data";
+import { Link } from "react-router-dom";
 
 const ProductPage = (props) => {
-  return <div>Product Page</div>;
+  const product = data.products.find(
+    (element) => element._id === parseInt(props.match.params.id)
+  );
+  return (
+    <Fragment>
+      <div className="back-to-results">
+        <Link to="/">Back to results</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt={product.name} />
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>
+              {product.rating} Stars ({product.numberOfReviews} Reviews)
+            </li>
+            <li>
+              <strong>{product.price}</strong>
+            </li>
+            <li>
+              <div>{product.description}</div>
+            </li>
+          </ul>
+        </div>
+        <div className="details-action">
+          <ul>
+            <li>
+              Price: <strong>${product.price}</strong>
+            </li>
+            <li>Status: {product.status}</li>
+            <li>
+              Qty:{" "}
+              <select name="qty" id="qty">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </li>
+            <li>
+              <button className="button primary">Add to Cart</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default ProductPage;
