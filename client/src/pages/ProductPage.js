62caf6529b8ad4e61e9475374b2a3307c75f905a
Claudio Rivera <me@claudiorivera.com>
2020-08-30 10:54:44 -0500

    product page renders qty amounts dynamically

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,57 +1,78 @@
-import React, { Fragment } from "react";
-import data from "../data";
+import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../actions/productActions";
 
 const ProductPage = (props) => {
-  const product = data.products.find(
-    (element) => element._id === parseInt(props.match.params.id)
-  );
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProductDetails(props.match.params.id));
+  }, []);
+
   return (
     <Fragment>
       <div className="back-to-results">
         <Link to="/">Back to results</Link>
       </div>
-      <div className="details">
-        <div className="details-image">
-          <img src={product.image} alt={product.name} />
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt={product.name} />
+          </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.rating} Stars ({product.numberOfReviews} Reviews)
+              </li>
+              <li>
+                <strong>${product.price}</strong>
+              </li>
+              <li>
+                <div>{product.description}</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>
+                Price: <strong>${product.price}</strong>
+              </li>
+              <li>Status: {product.status}</li>
+              <li>
+                Qty:
+                <select
+                  value={qty}
+                  name="qty"
+                  id="qty"
+                  onChange={(event) => {
+                    setQty(parseInt(event.target.value));
+                  }}
+                >
+                  {[...Array(product.qtyInStock).keys()].map((index) => (
+                    <option key={index} value={index + 1}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+              </li>
+              <li>
+                <button className="button primary">Add to Cart</button>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="details-info">
-          <ul>
-            <li>
-              <h4>{product.name}</h4>
-            </li>
-            <li>
-              {product.rating} Stars ({product.numberOfReviews} Reviews)
-            </li>
-            <li>
-              <strong>{product.price}</strong>
-            </li>
-            <li>
-              <div>{product.description}</div>
-            </li>
-          </ul>
-        </div>
-        <div className="details-action">
-          <ul>
-            <li>
-              Price: <strong>${product.price}</strong>
-            </li>
-            <li>Status: {product.status}</li>
-            <li>
-              Qty:{" "}
-              <select name="qty" id="qty">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-              </select>
-            </li>
-            <li>
-              <button className="button primary">Add to Cart</button>
-            </li>
-          </ul>
-        </div>
-      </div>
+      )}
     </Fragment>
   );
 };
