210fd99d4708fe7a254eac881498186a95026a49
Claudio Rivera <me@claudiorivera.com>
2020-08-30 10:53:39 -0500

    added api end point for product details

--- server/server.js
+++ server/server.js
@@ -8,6 +8,18 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find(
+    (product) => product._id === parseInt(productId)
+  );
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
