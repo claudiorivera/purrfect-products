548db06b169dd2b2190f3466927d0136cb17576d
Claudio Rivera <me@claudiorivera.com>
2020-09-06 11:27:28 -0500

    Cart styling

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../actions/cartActions";
+import { Link } from "react-router-dom";
 
 const Cart = (props) => {
   const cart = useSelector((state) => state.cart);
@@ -19,6 +20,11 @@ const Cart = (props) => {
     }
   }, []);
 
+  const handleRemoveFromCart = (productId) => {
+    // dispatch(removeFromCart(productId));
+    console.log(`removing ${productId}`);
+  };
+
   return (
     <div className="cart">
       <div className="cart-list">
@@ -31,10 +37,14 @@ const Cart = (props) => {
             <div>Cart is empty</div>
           ) : (
             cartItems.map((item) => (
-              <div>
-                <img src={item.image} alt={item.name} />
+              <li key={item.product}>
+                <div className="cart-image">
+                  <img src={item.image} alt={item.name} />
+                </div>
                 <div className="cart-name">
-                  <div>{item.name}</div>
+                  <div>
+                    <Link to={`/products/${item.product}`}>{item.name}</Link>
+                  </div>
                   <div>
                     Qty:
                     <select name="qty" id="qty">
@@ -42,15 +52,35 @@ const Cart = (props) => {
                       <option value="2">2</option>
                       <option value="3">3</option>
                     </select>
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={() => handleRemoveFromCart(item.product)}
+                    >
+                      Remove From Cart
+                    </button>
                   </div>
                 </div>
-                <div>{item.price}</div>
-              </div>
+                <div className="cart-price">${item.price}</div>
+              </li>
             ))
           )}
         </ul>
       </div>
-      <div className="cart-action"></div>
+      <div className="cart-action">
+        <h3>
+          Subtotal (
+          {cartItems.reduce((prevItem, curItem) => prevItem + curItem.qty, 0)}{" "}
+          items): $
+          {cartItems.reduce(
+            (prevItem, curItem) => prevItem + curItem.price * curItem.qty,
+            0
+          )}
+        </h3>
+        <button className="button primary" disabled={cartItems.length === 0}>
+          Proceed to Checkout
+        </button>
+      </div>
     </div>
   );
 };
