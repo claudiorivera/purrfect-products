765acfa634498764126e8bf99d9f4901773b9eec
Claudio Rivera <me@claudiorivera.com>
2020-09-07 17:24:25 -0500

    Moved CSS import to top of file for consistency

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
 import "./App.css";
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
