e962b2740e654c4ba34b09e85ab82fcf6ce549e8
Claudio Rivera <me@claudiorivera.com>
2020-09-07 17:24:40 -0500

    Move CSS import to top

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
+import "./index.css";
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import store from "./store";
 
