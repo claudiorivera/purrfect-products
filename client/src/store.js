d6edf5f26663fb6a7e58143a25c91c5088bc6a89
Claudio Rivera <me@claudiorivera.com>
2020-09-07 17:24:54 -0500

    Add users reducer

--- client/src/store.js
+++ client/src/store.js
@@ -4,17 +4,20 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { usersReducer } from "./reducers/usersReducers";
 import Cookie from "js-cookie";
 
 const cartItems = Cookie.getJSON("cartItems") || [];
+const user = Cookie.getJSON("user") || null;
 
 const store = configureStore({
   reducer: {
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    users: usersReducer,
   },
-  preloadedState: { cart: { cartItems } },
+  preloadedState: { cart: { cartItems }, users: { user } },
 });
 
 export default store;
