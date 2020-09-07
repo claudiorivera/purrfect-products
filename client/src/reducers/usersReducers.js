710b476eba23150257948826e698a456e2502823
Claudio Rivera <me@claudiorivera.com>
2020-09-07 17:26:13 -0500

    Create users reducer

---
+++ client/src/reducers/usersReducers.js
@@ -0,0 +1,23 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+const usersReducer = createReducer(
+  {
+    user: {},
+    loading: false,
+  },
+  {
+    LOGIN_USER_REQUEST: (state, action) => {
+      state.loading = true;
+    },
+    LOGIN_USER_SUCCESSFUL: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    },
+    LOGIN_USER_FAILED: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  }
+);
+
+export { usersReducer };
