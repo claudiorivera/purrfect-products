a253f4e8a0a40f27e697eb4fbc24735a8a8505f5
Claudio Rivera <me@claudiorivera.com>
2020-09-08 14:25:46 -0500

    Create user slice

---
+++ client/src/features/user/userSlice.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import Cookie from "js-cookie";
+
+const user = Cookie.getJSON("user") || {};
+
+export const login = createAsyncThunk(
+  "userAuth/loginStatus",
+  async (args, { getState, requestId }) => {
+    const { currentRequestId, loading } = getState().userAuth;
+    if (loading !== "pending" || requestId !== currentRequestId) {
+      return;
+    }
+    const { data } = await axios.post("/api/users/login", {
+      email: args.email,
+      password: args.password,
+    });
+    return data;
+  }
+);
+
+const cartSlice = createSlice({
+  name: "userAuth",
+  initialState: {
+    user,
+    loading: "idle",
+    currentRequestId: undefined,
+    isLoggedIn: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: {
+    [login.pending]: (state, action) => {
+      if (state.loading === "idle") {
+        state.loading = "pending";
+        state.currentRequestId = action.meta.requestId;
+      }
+    },
+    [login.fulfilled]: (state, action) => {
+      const { requestId } = action.meta;
+      if (state.loading === "pending" && state.currentRequestId === requestId) {
+        state.loading = "idle";
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        Cookie.set("user", JSON.stringify(state.user));
+        state.currentRequestId = undefined;
+      }
+    },
+    [login.rejected]: (state, action) => {
+      const { requestId } = action.meta;
+      if (state.loading === "pending" && state.currentRequestId === requestId) {
+        state.loading = "idle";
+        state.isLoggedIn = false;
+        state.error = action.error;
+        state.currentRequestId = undefined;
+      }
+    },
+  },
+});
+
+const { reducer } = cartSlice;
+export default reducer;
