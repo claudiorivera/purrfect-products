c703b8e5c0ab5b86bb1b05db71142e4b188dccac
Claudio Rivera <me@claudiorivera.com>
2020-09-09 17:11:36 -0500

    Add register page

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -5,6 +5,7 @@ import ProductList from "../features/products/pages/ProductList";
 import ProductDetails from "../features/products/pages/ProductDetails";
 import Cart from "../features/cart/pages/Cart";
 import Login from "../features/user/pages/Login";
+import Register from "../features/user/pages/Register";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -18,6 +19,7 @@ const App = () => {
         <main className="main">
           <div className="content">
             <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
             <Route path="/products/:id" component={ProductDetails} />
             <Route path="/cart/:id?" component={Cart} />
             <Route path="/" component={ProductList} exact />
---
+++ client/src/features/user/pages/Register.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { register } from "../userSlice";
+import "./Register.css";
+
+const Register = (props) => {
+  const [email, setEmail] = useState(null);
+  const [name, setName] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const { loading, error, isLoggedIn } = auth;
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      props.history.push("/");
+    }
+  }, [isLoggedIn, props.history]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(register({ name, email, password }));
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="register-form">
+        <ul>
+          <li>
+            <h2>Register</h2>
+          </li>
+          <li>
+            {loading === "pending" && <div>Loading...</div>}
+            {error && <div>{error.message}</div>}
+          </li>
+          <li>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              onChange={(e) => setName(e.target.value)}
+            />
+          </li>
+          <li>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </li>
+          <li>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </li>
+          <li>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              required
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </li>
+          <li>
+            <button
+              type="submit"
+              className="button primary"
+              disabled={
+                password !== confirmPassword || !name || !email || !password
+              }
+            >
+              Register
+            </button>
+          </li>
+          <li>
+            <Link to="/login" className="full-width text-center">
+              Already registered? Login here.
+            </Link>
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
