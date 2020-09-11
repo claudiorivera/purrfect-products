44122cc972fccb4737afcf88e1cc059b78776356
Claudio Rivera <me@claudiorivera.com>
2020-09-10 21:13:50 -0500

    Add auth utility functions for future use

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-module.exports = (user) => {
+const getToken = (user) => {
   const { _id, name, email, isAdmin } = user;
   jwt.sign(
     {
@@ -15,3 +15,26 @@ module.exports = (user) => {
     }
   );
 };
+
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const onlyToken = token.slice(7, token.length);
+    jwt.verify(onlyToken, process.env.JWT_SECRET, (error, decode) => {
+      if (error) return res.status(401).send({ message: "Invalid token" });
+      req.user = token;
+      return next();
+    });
+  }
+  req.status(401).send({ message: "Token missing" });
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user?.isAdmin) {
+    return next();
+  } else {
+    return res.status(401).send({ message: "Admin token is invalid" });
+  }
+};
+
+export { getToken, isAuth, isAdmin };
