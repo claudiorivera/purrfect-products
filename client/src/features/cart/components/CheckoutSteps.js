3bde2e103705ab12ca39afd7f6acd2c5db4ace65
Claudio Rivera <me@claudiorivera.com>
2020-09-20 10:21:44 -0500

    Checkout steps and styling

---
+++ client/src/features/cart/components/CheckoutSteps.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CheckoutSteps.css";
+
+const CheckoutSteps = (props) => {
+  return (
+    <div className="checkout-steps">
+      <div className={props.step1 ? "active" : ""}>Login</div>
+      <div className={props.step2 ? "active" : ""}>Shipping</div>
+      <div className={props.step3 ? "active" : ""}>Payment</div>
+      <div className={props.step4 ? "active" : ""}>Confirm Order</div>
+    </div>
+  );
+};
+
+export default CheckoutSteps;
--- client/src/features/cart/pages/Checkout.js
+++ client/src/features/cart/pages/Checkout.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { saveShippingInfo } from "../cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 import "./Checkout.css";
 
-const Checkout = () => {
+const Checkout = (props) => {
   const [address, setAddress] = useState(null);
   const [address2, setAddress2] = useState(null);
   const [city, setCity] = useState(null);
@@ -14,75 +15,79 @@ const Checkout = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(saveShippingInfo({ address, address2, city, state, postalCode }));
+    props.history.push("/payment");
   };
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit} className="checkout-form">
-        <ul>
-          <li>
-            <h2>Checkout</h2>
-          </li>
-          <li>
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              name="address"
-              id="address"
-              required
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </li>
-          <li>
-            <label htmlFor="address2">Address Line 2 (Optional)</label>
-            <input
-              type="text"
-              name="address2"
-              id="address2"
-              onChange={(e) => setAddress2(e.target.value)}
-            />
-          </li>
-          <li>
-            <label htmlFor="city">City</label>
-            <input
-              type="text"
-              name="city"
-              id="city"
-              required
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </li>
-          <li>
-            <label htmlFor="state">State</label>
-            <input
-              type="text"
-              name="state"
-              id="state"
-              required
-              onChange={(e) => setState(e.target.value)}
-            />
-          </li>
-          <li>
-            <label htmlFor="postalCode">Postal Code</label>
-            <input
-              type="text"
-              name="postalCode"
-              id="postalCode"
-              required
-              onChange={(e) => setPostalCode(e.target.value)}
-            />
-          </li>
-          <li>
-            <button
-              type="submit"
-              className="button primary"
-              disabled={!address || !city || !state || !postalCode}
-            >
-              Continue
-            </button>
-          </li>
-        </ul>
-      </form>
+    <div>
+      <CheckoutSteps step1 step2 />
+      <div className="form-container">
+        <form onSubmit={handleSubmit} className="checkout-form">
+          <ul>
+            <li>
+              <h2>Checkout</h2>
+            </li>
+            <li>
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                required
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </li>
+            <li>
+              <label htmlFor="address2">Address Line 2 (Optional)</label>
+              <input
+                type="text"
+                name="address2"
+                id="address2"
+                onChange={(e) => setAddress2(e.target.value)}
+              />
+            </li>
+            <li>
+              <label htmlFor="city">City</label>
+              <input
+                type="text"
+                name="city"
+                id="city"
+                required
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </li>
+            <li>
+              <label htmlFor="state">State</label>
+              <input
+                type="text"
+                name="state"
+                id="state"
+                required
+                onChange={(e) => setState(e.target.value)}
+              />
+            </li>
+            <li>
+              <label htmlFor="postalCode">Postal Code</label>
+              <input
+                type="text"
+                name="postalCode"
+                id="postalCode"
+                required
+                onChange={(e) => setPostalCode(e.target.value)}
+              />
+            </li>
+            <li>
+              <button
+                type="submit"
+                className="button primary"
+                disabled={!address || !city || !state || !postalCode}
+              >
+                Continue
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
   );
 };
