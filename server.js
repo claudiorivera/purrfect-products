9c80ba9fd4aef39810b35e9397ff0e958811ef14
Claudio Rivera <me@claudiorivera.com>
2020-10-05 17:03:43 -0500

    For Heroku deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -28,6 +29,11 @@ app.use(express.json());
 // Routes
 app.use("/api/users", require("./api/users"));
 app.use("/api/products", require("./api/products"));
+
+// If we're in production, serve the client/build folder
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
