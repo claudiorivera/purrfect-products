a0f0a3be420aaf2dd57f345457a9cb443d370fba
Claudio Rivera <me@claudiorivera.com>
2021-07-27 21:30:10 -0500

    🐛 Fix 404 routes

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.use("/api/products", require("./api/products"));
 // If we're in production, serve the client/build folder
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
